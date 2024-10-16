2a42be8d604226cda3de5db77ac0fb5e4f58b7d8
refaelach <rephaell@gmail.com>
2024-10-16 14:28:09 +0300

    Add New Feature link to Header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,14 +3,15 @@ import React from 'react';
 const Header = () => {
   return (
     <header style={{backgroundColor: '#f0f0f0', padding: '1rem'}}>
-      <h1>Welcome to PAPI</h1>
-      <nav>
-        <ul style={{listStyle: 'none', padding: 0}}>
-          <li style={{display: 'inline', marginRight: '1rem'}}><a href="/">Home</a></li>
-          <li style={{display: 'inline', marginRight: '1rem'}}><a href="/about">About</a></li>
-        </ul>
-      </nav>
-    </header>
+  <h1>Welcome to PAPI</h1>
+  <nav>
+    <ul style={{listStyle: 'none', padding: 0}}>
+      <li style={{display: 'inline', marginRight: '1rem'}}><a href="/">Home</a></li>
+      <li style={{display: 'inline', marginRight: '1rem'}}><a href="/about">About</a></li>
+      <li style={{display: 'inline', marginRight: '1rem'}}><a href="/new-feature">New Feature</a></li>
+    </ul>
+  </nav>
+</header>
   );
 };
 
